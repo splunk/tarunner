8f7783f0e9033bf39f726526842805e14c0f3d78
Antoine Toulme <atoulme@splunk.com>
2026-07-14 21:55:11 -0700

    fix path

--- internal/receiver/monitorreceiver/receiver_test.go
+++ internal/receiver/monitorreceiver/receiver_test.go
@@ -32,7 +32,7 @@ func TestReadFile(t *testing.T) {
 		Input: conf.Input{
 			Configuration: conf.Configuration{
 				Stanza: conf.Stanza{
-					Name: fmt.Sprintf("monitor://%s", filepath.Join(tempDir, "foo.txt")),
+					Name: fmt.Sprintf("monitor://%s%c%s", tempDir, filepath.Separator, "foo.txt"),
 					App:  "",
 					Params: conf.Params{
 						conf.Param{
